c1dfddf1f26742b95e65dbf306307c76e910bcd8
motwanikapil <kapilmotwani1010@gmail.com>
2026-03-08 21:36:01 +0530

    practicing fundme.sol

--- FundMe.sol
+++ FundMe.sol
@@ -1,18 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract FundMe {
-    // uint256 public myValue = 1;
-    uint256 public minimumUsd = 5;
-    function fund() public payable {
-        // msg.value is a global in solidity smart contract
-        // myValue = myValue + 2;
-        // require(msg.value > 1e18, "didn't send enough eth");
-        require(msg.value >= minimumUsd, "didn't send enough eth");
+import {AggregatorV3Interface} from "@chainlink/contracts@1.5.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-        // what is a revert?
-        // undo any actions that have been done, and send the remaining gas back
+contract FundMe {
+    uint256 public minimumUsd = 5e18;
+
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough eth");
     }
 
-    // function withdraw() public {}
+    function getPrice() public view returns(uint256){
+        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // abi 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // (uint80 roundId, int256 price, uint256 startedAt, uint256 timestamp, uint256 answeredInRound) =  priceFeed.latestRoundData();
+        (,int256 price,,,) =  priceFeed.latestRoundData();
+        // price of eth in term of usd
+        return uint256(price * 1e10); // because to format the price correctly
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
 }
